4e0b17fee16a8bf4b7ba5097c93c2fc8bcc9ce61
Max-Rus1977 <maxsvet77@yandex.ru>
2021-07-25 17:54:48 +0300

    Удаление товара из козины

--- js/comond.js
+++ js/comond.js
@@ -2,17 +2,14 @@ const productMinus = document.querySelectorAll('.product-minus-js');
 const productPlus = document.querySelectorAll('.product-plus-js');
 
 
-
 function increaseAmountProduct() {
 
   const thisProductCounter = this.closest('.wrapper-product__counter');
   const productAmount = thisProductCounter.querySelector('.product-amount-js');
-  console.log(productAmount);
 
   let productAmountData = productAmount.textContent;
 
   productAmount.innerText = ++productAmountData;
-  console.log(productAmountData);
 
   function clearClass() {
     productAmount.classList.remove('amount-bounce');
@@ -47,3 +44,47 @@ for (let plus of productPlus) {
 for (let minus of productMinus) {
   minus.addEventListener('click', subtractionAmountProduct);
 }
+
+/* Добавление товара в корзину */
+
+const asideCart = document.querySelector('.aside-cart-js');
+const btnCarts = document.querySelectorAll('.btn-cart');
+
+function addTooCart() {
+  const thisProductCard = this.closest('.product');
+  const clonThisProductCard = thisProductCard.cloneNode(true);
+
+  clonThisProductCard.classList.add('cart-product');
+  clonThisProductCard.querySelector('.btn-cart').remove();
+  clonThisProductCard.insertAdjacentHTML('afterbegin', '<div class="close-cartProduct">&#10060;</div>');
+  const a = clonThisProductCard.querySelector('.close-cartProduct');
+
+  a.addEventListener('click', () => {
+    a.closest('.cart-product').remove();
+
+  });
+
+
+  // clonThisProductCard.prepend(removeProduct);
+
+  asideCart.appendChild(clonThisProductCard);
+
+
+}
+
+for (let btnCart of btnCarts) {
+  btnCart.addEventListener('click', addTooCart);
+}
+
+const divCloseCartProduct = asideCart.getElementsByClassName('close-cartProduct');
+console.log(divCloseCartProduct);
+
+for (let divClose of divCloseCartProduct) {
+  divClose.addEventListener('click', function () {
+    // clonThisProductCard.remove();
+    console.log(this.divClose);
+
+  })
+}
+
+//asideCart.getElementsByClassName
